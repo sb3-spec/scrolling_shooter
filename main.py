69678497dd5f5b48a80e8fc78682f17782481eeb
Robert Sheffield <sheffira2@gmail.com>
2021-09-29 14:22:58 -0700

    Added running and idle animations, player model flips when direction changes

--- main.py
+++ main.py
@@ -12,22 +12,51 @@ display.set_caption("2D Shooter")
 clock = pygame.time.Clock()
 FPS = 60
 
+# define player variables
+
+GRAVITY = .75
+
 BG = (144, 201, 120)  # Background colors
+RED = (255, 0, 0)
 
 moving_left = False
 moving_right = False
 
 class Character(Sprite):
     """Basic class for the hero, serves as a blueprint for our characters"""
-    def __init__(self, x, y, scale, speed=10):
+    def __init__(self, char_type, x, y, scale, speed=10):
         Sprite.__init__(self)
 
         self.speed = speed
         self.x = x
         self.y = y
+        self.char_type = char_type
+        self.flip = False
+        self.action = 0
+        self.frame_index = 0
+        self.animation_list = []
+        self.jump = False
+        self.alive = True
+        self.vel_y = 0
+        self.update_time = pygame.time.get_ticks()
 
-        img = pygame.image.load('assets/mario.png')
-        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        
+        temp_list = []
+        for i in range(5):
+            img = pygame.image.load(f'./assets/{self.char_type}/Idle/{i}.png')
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+
+        temp_list = []
+        for i in range(6):
+            img = pygame.image.load(f'./assets/{self.char_type}/Running/{i}.png')
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        self.image = self.animation_list[self.action][self.frame_index]
+
+
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
@@ -37,13 +66,35 @@ class Character(Sprite):
 
         if moving_left:
             dx = -self.speed
+            self.flip = True
         if moving_right:
             dx = self.speed
+            self.flip = False
         self.rect.x += dx
         self.rect.y += dy
 
+    def update_animation(self):
+        # Update animation
+        ANIMATION_COOLDOWN = 100
+        #update image depending on current time
+        self.image = self.animation_list[self.action][self.frame_index]
+
+        #check if enough time has passed since last update
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+            if self.frame_index == len(self.animation_list[self.action]):
+                self.frame_index = 0
+
+    def update_action(self, new_action):
+        if new_action != self.action:
+            self.action = new_action
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
+
     def draw(self):
-        screen.blit(self.image, self.rect)
+        flipped = pygame.transform.flip(self.image, self.flip, False)
+        screen.blit(flipped, self.rect)
 
     def jump(self):
         dy = 5
@@ -51,9 +102,10 @@ class Character(Sprite):
 
 def draw_bg():
     screen.fill(BG)
+    pygame.draw.line(screen, RED, (0, 300), (SCREEN_WIDTH, 300))
 
 
-hero = Character(200, 600, .05)  
+player = Character("player", 200, 600, 2)  
 
 """Game loop that is checking for user input"""
 running = True
@@ -61,8 +113,16 @@ while running:
 
     clock.tick(FPS)
     draw_bg()
-    hero.draw()  # draws the hero's model to the screen
-    hero.move(moving_left, moving_right)
+    player.update_animation()
+    player.draw()  # draws the player's model to the screen
+    player.move(moving_left, moving_right)
+
+    # update player actions
+    if player.alive:
+        if moving_left or moving_right:
+            player.update_action(1) # 1 is running
+        else:
+            player.update_action(0) # 0 is idle
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -74,6 +134,8 @@ while running:
                 moving_right = True
             elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
                 moving_left = True
+            elif event.key == pygame.K_SPACE and player.alive:
+                player.jump = True
             elif event.key == pygame.K_ESCAPE:
                 running = False
 
@@ -83,6 +145,8 @@ while running:
                 moving_right = False
             elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
                 moving_left = False
+            elif event.key == pygame.K_SPACE:
+                player.jump = False
 
     display.update()  # Refreshes the screen
 
